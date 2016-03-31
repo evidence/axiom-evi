c15acd39b95a248979a298a69d563ceb3b2662e2
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-03-31 17:29:46 +0200

    fisrt implementation of axiom qemu emulation works

--- scripts/start_qemu.sh
+++ scripts/start_qemu.sh
@@ -13,4 +13,6 @@ set -x
 
 #${QEMU} -M vexpress-a9 -m 512M -kernel $BUILDROOT/output/images/zImage -dtb $BUILDROOT/output/images/vexpress-v2p-ca9.dtb -drive file=$BUILDROOT/output/images/rootfs.ext2,if=sd,format=raw -append "console=ttyAMA0,115200 root=/dev/mmcblk0 mem=256M" -serial mon:stdio  -nographic -net nic,model=lan9118 -net user,hostfwd=tcp::20001-:22 -monitor tcp::10001,server,nowait,nodelay -device axiom-dev
 
-${QEMU} -M arm-generic-fdt-plnx -machine linux=on -serial /dev/null -serial mon:stdio -display none -kernel $BUILDROOT/output/images/uImage -dtb $BUILDROOT/output/images/zynq-zc706.dtb --initrd $BUILDROOT/output/images/rootfs.cpio.uboot -net nic,vlan=0 -net user,vlan=0 -net nic,vlan=1 -net none,vlan=1 -net nic,vlan=2 -net tap,ifname=tap0,vlan=2 -append "mem=512M"
+#${QEMU} -M arm-generic-fdt-plnx -machine linux=on -serial /dev/null -serial mon:stdio -display none -kernel $BUILDROOT/output/images/uImage -dtb $BUILDROOT/output/images/zynq-zc706.dtb --initrd $BUILDROOT/output/images/rootfs.cpio.uboot -net nic,vlan=0 -net user,vlan=0 -net nic,vlan=1 -net none,vlan=1 -net nic,vlan=2 -net tap,ifname=tap0,vlan=2 -append "mem=512M" -redir tcp:1234::1234
+
+${QEMU} -M arm-generic-fdt-plnx -machine linux=on -serial /dev/null -serial mon:stdio -display none -kernel $BUILDROOT/output/images/uImage -dtb $BUILDROOT/output/images/zynq-zc706.dtb --initrd $BUILDROOT/output/images/rootfs.cpio.uboot -net nic,vlan=0 -net user,vlan=0 -net nic,vlan=1 -net none,vlan=1 -net nic,vlan=2 -net socket,vlan=2,connect=localhost:33300 -append "mem=512M" -redir tcp:1234::1234
