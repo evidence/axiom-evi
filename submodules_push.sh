676e0c85dde6822f939b49b19714e7c47e3abef6
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-04-05 15:23:03 +0200

    small fixes previous axiom-v0.3 tag

--- submodules_push.sh
+++ submodules_push.sh
@@ -4,8 +4,8 @@ set -x
 
 ## This script push commit by all submodules
 
-git push
+git push --tags
 
 #git submodule foreach --recursive \
 git submodule foreach \
-    'git push || :'
+    'git push --tags || :'
