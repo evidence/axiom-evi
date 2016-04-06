9d279435ea771ec4709b6b264f20cac14a29b876
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-04-06 13:03:27 +0200

    Merge axiom-max

--- submodules_pull.sh
+++ submodules_pull.sh
@@ -4,8 +4,8 @@ set -x
 
 ## This script push commit by all submodules
 
-git pull
+git pull --rebase
 
 #git submodule foreach --recursive \
 git submodule foreach \
-    'git pull || :'
+    'git pull --rebase || :'
