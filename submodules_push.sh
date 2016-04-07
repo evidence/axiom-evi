53e26c357e6a7269f89bee4452e08a8a99561c41
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-04-07 11:03:24 +0200

    fix submodules_push.sh

--- submodules_push.sh
+++ submodules_push.sh
@@ -4,8 +4,8 @@ set -x
 
 ## This script push commit by all submodules
 
-git push --tags $1
+git push $1
 
 #git submodule foreach --recursive \
 git submodule foreach \
-    "git push --tags $1 || :"
+    "git push $1 || :"
