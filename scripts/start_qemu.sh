c0daaa420bbab5b95ef26888ddda8784b5a710a6
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-07-21 17:34:46 +0200

    start_qemu.sh: remove replicated code

--- scripts/start_qemu.sh
+++ scripts/start_qemu.sh
@@ -15,6 +15,16 @@ SERIAL_CONSOLE="-serial mon:stdio -display none"
 CONSOLE=0
 NETWORK=${NETWORK:-0}
 
+TAPNAME=tapax
+MAC=1a:46:0b:ca:bc
+SYSNET=/sys/devices/virtual/net
+function if2dev {
+    ls -d $SYSNET/$1/tap* | sed 's,.*tap,/dev/tap,'
+}
+function if2exist {
+    [ -d $SYSNET/$1 ]
+}
+
 function usage
 {
     echo -e "usage: ./start_qemu.sh [OPTION...]"
@@ -26,16 +36,6 @@ function usage
     echo -e " -c, --console               use stdio console"
     echo -e " -n, --network               create a tap network interface for guest comunication"
     echo -e " -h, --help                  print this help"
-}
-
-TAPNAME=tapax
-MAC=1a:46:0b:ca:bc
-SYSNET=/sys/devices/virtual/net
-function if2dev {
-    ls -d $SYSNET/$1/tap* | sed 's,.*tap,/dev/tap,'
-}
-function if2exist {
-    [ -d $SYSNET/$1 ]
 }
 
 while [ "$1" != "" ]; do
@@ -82,14 +82,15 @@ fi
 NETLINE="-net nic,vlan=1 -net none,vlan=1"
 if  [ $NETWORK -eq 1 ]; then
     if2exist $TAPNAME$ID || { echo "interface $TAPNAME$ID does not exist! have \
-        you run 'make netup' on host?"; exit 255; }
-    NETLINE="-net nic,macaddr=$MAC:$ID,vlan=3 -net tap,vlan=3,fd=42"
+you run 'make netup' on host?"; exit 255; }
+    NETLINE="-net nic,macaddr=$MAC:$ID,vlan=3 -net tap,vlan=3,fd=42 	\
+        42<>$(if2dev $TAPNAME$ID)"
 fi
 
 set -x
 
 if [ "$BOARD" = "zynq" ]; then
-    ${QEMU} -M arm-generic-fdt-plnx -machine linux=on -m 256            \
+    eval ${QEMU} -M arm-generic-fdt-plnx -machine linux=on -m 256       \
     -serial /dev/null ${SERIAL} -kernel ${IMAGES}/uImage                \
     -dtb ${IMAGES}/zynq-zc706.dtb --initrd ${IMAGES}/rootfs.cpio.uboot  \
     -net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0         \
@@ -97,35 +98,19 @@ if [ "$BOARD" = "zynq" ]; then
     -net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}      \
     -append "mem=256M" -chardev vc,id=char1 -redir tcp:1234{ID}::1234
 elif [ "$BOARD" = "zynqmp" ]; then
-    if [ $NETWORK -eq 0 ]; then
-	${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null  \
-	-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4       \
-	-device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0              \
-	-device loader,file=${IMAGES}/Image,addr=0x00080000             \
-	-device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000 \
-	-device loader,file=${IMAGES}/u-boot.elf                        \
-	-hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                \
-	-net nic,vlan=1 -net none,vlan=1                                \
-	-net nic,vlan=1 -net none,vlan=1                                \
-	-net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0     \
-	$NETLINE                                                        \
-	-net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}  \
-	-chardev vc,id=char1 -redir tcp:1234${ID}::1234
-    else
-	${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null  \
-	-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4       \
-	-device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0              \
-	-device loader,file=${IMAGES}/Image,addr=0x00080000             \
-	-device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000 \
-	-device loader,file=${IMAGES}/u-boot.elf                        \
-	-hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                \
-	-net nic,vlan=1 -net none,vlan=1                                \
-	-net nic,vlan=1 -net none,vlan=1                                \
-	-net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0     \
-	$NETLINE 42<>$(if2dev $TAPNAME$ID)                              \
-	-net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}  \
-	-chardev vc,id=char1 -redir tcp:1234${ID}::1234
-    fi
+    eval ${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null \
+    -device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4           \
+    -device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0                  \
+    -device loader,file=${IMAGES}/Image,addr=0x00080000                 \
+    -device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000     \
+    -device loader,file=${IMAGES}/u-boot.elf                            \
+    -hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                    \
+    -net nic,vlan=1 -net none,vlan=1                                    \
+    -net nic,vlan=1 -net none,vlan=1                                    \
+    -net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0         \
+    $NETLINE                                                            \
+    -net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}      \
+    -chardev vc,id=char1 -redir tcp:1234${ID}::1234
 else
     echo "Board $BOARD not supported"
     usage
