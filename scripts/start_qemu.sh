35c192c97a7da2ba4e300696bac8bcad6098bac9
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-07-21 17:15:18 +0200

    start_qemu.sh: fix style

--- scripts/start_qemu.sh
+++ scripts/start_qemu.sh
@@ -79,11 +79,10 @@ else
     SERIAL=$SERIAL_VIDEO
 fi
 
-echo PIPPO
-
 NETLINE="-net nic,vlan=1 -net none,vlan=1"
 if  [ $NETWORK -eq 1 ]; then
-    if2exist $TAPNAME$ID || { echo "interface $TAPNAME$ID does not exist! have you run 'make netup' on host?"; exit 255; }   
+    if2exist $TAPNAME$ID || { echo "interface $TAPNAME$ID does not exist! have \
+        you run 'make netup' on host?"; exit 255; }
     NETLINE="-net nic,macaddr=$MAC:$ID,vlan=3 -net tap,vlan=3,fd=42"
 fi
 
@@ -99,33 +98,33 @@ if [ "$BOARD" = "zynq" ]; then
     -append "mem=256M" -chardev vc,id=char1 -redir tcp:1234{ID}::1234
 elif [ "$BOARD" = "zynqmp" ]; then
     if [ $NETWORK -eq 0 ]; then
-	${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null      \
-		-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4           \
-		-device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0                  \
-		-device loader,file=${IMAGES}/Image,addr=0x00080000                 \
-		-device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000     \
-		-device loader,file=${IMAGES}/u-boot.elf                            \
-		-hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                    \
-		-net nic,vlan=1 -net none,vlan=1   \
-		-net nic,vlan=1 -net none,vlan=1   \
-		-net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0         \
-		$NETLINE \
-		-net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}      \
-		-chardev vc,id=char1 -redir tcp:1234${ID}::1234
+	${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null  \
+	-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4       \
+	-device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0              \
+	-device loader,file=${IMAGES}/Image,addr=0x00080000             \
+	-device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000 \
+	-device loader,file=${IMAGES}/u-boot.elf                        \
+	-hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                \
+	-net nic,vlan=1 -net none,vlan=1                                \
+	-net nic,vlan=1 -net none,vlan=1                                \
+	-net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0     \
+	$NETLINE                                                        \
+	-net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}  \
+	-chardev vc,id=char1 -redir tcp:1234${ID}::1234
     else
-	${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null      \
-		-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4           \
-		-device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0                  \
-		-device loader,file=${IMAGES}/Image,addr=0x00080000                 \
-		-device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000     \
-		-device loader,file=${IMAGES}/u-boot.elf                            \
-		-hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                    \
-		-net nic,vlan=1 -net none,vlan=1   \
-		-net nic,vlan=1 -net none,vlan=1   \
-		-net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0         \
-		$NETLINE 42<>$(if2dev $TAPNAME$ID) \
-		-net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}      \
-		-chardev vc,id=char1 -redir tcp:1234${ID}::1234
+	${QEMU} -M arm-generic-fdt -m 256M ${SERIAL} -serial /dev/null  \
+	-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4       \
+	-device loader,file=${ARM_FIRMWARE}/bl31.elf,cpu=0              \
+	-device loader,file=${IMAGES}/Image,addr=0x00080000             \
+	-device loader,file=${IMAGES}/zynqmp-zcu102.dtb,addr=0x04080000 \
+	-device loader,file=${IMAGES}/u-boot.elf                        \
+	-hw-dtb ${QEMU_DTS}/${AXIOM_DTB}                                \
+	-net nic,vlan=1 -net none,vlan=1                                \
+	-net nic,vlan=1 -net none,vlan=1                                \
+	-net nic,vlan=0 -net user,hostfwd=tcp::2220${ID}-:22,vlan=0     \
+	$NETLINE 42<>$(if2dev $TAPNAME$ID)                              \
+	-net nic,vlan=2 -net socket,vlan=2,connect=localhost:3330${ID}  \
+	-chardev vc,id=char1 -redir tcp:1234${ID}::1234
     fi
 else
     echo "Board $BOARD not supported"
