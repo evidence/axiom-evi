8b40eac6bb3bca99097180610f49030b01c81bf3
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-10-28 17:24:15 +0200

    Add API to require private NIC memory

--- scripts/buildroot/overlay/root/axiom-startup.sh
+++ scripts/buildroot/overlay/root/axiom-startup.sh
@@ -3,4 +3,4 @@
 echo 1 > /proc/sys/vm/overcommit_memory
 echo 0 > /proc/sys/kernel/randomize_va_space
 
-modprobe axiom-netdev && modprobe axiom_mem_dev && axiom-init $1 &
+modprobe axiom-netdev && axiom-init $1 &
