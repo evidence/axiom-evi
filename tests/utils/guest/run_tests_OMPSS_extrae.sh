bebbd6849237c8c2b78fd1db5c3646e20e33c1cf
Stefano Garzarella <s.garzarella@evidence.eu.com>
2017-01-18 11:10:52 +0100

    add initial support for extrae

---
+++ tests/utils/guest/run_tests_OMPSS_extrae.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=/usr/lib/instrumentation
+export LD_PRELOAD=/usr/lib/libnanostrace.so
+
+set -x
+
+NX_ARGS="--cluster --cluster-network=axiom --smp-workers=1 --instrumentation=extrae" \
+       NX_CLUSTER_NODE_MEMORY=16777216 \
+       NX_GASNET_SEGMENT_SIZE=67108864 \
+       NX_EXTRAE_AVOID_MERGE=1 \
+       EXTRAE_CONFIG_FILE=extrae.xml \
+       EXTRAE_HOME=/usr \
+       axiom-run -P ompss $@
