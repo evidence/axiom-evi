c3a0c9cbaa58e4c051f174f47f7e1d29e227bb05
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-01-25 09:59:46 +0100

    tests: script to run OMPSS n-body test

---
+++ tests/ompss/run_test_nbody.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ x$1 == x ]; then
+   echo "Us: $0" '<particles input file>'
+else
+
+ARG1=`echo $1 | sed -e 's/[a-z]*-//' -e 's/-.*//'`
+ARG3=`echo $1 |sed -e 's/.*-//' -e 's/\..*//'`
+    
+echo "Executing N-body on `axiom-info -N -q` boards..."
+
+NUM_TH=3
+
+#
+#
+#
+
+export NX_ARGS="--cluster --cluster-network axiom --smp-workers $NUM_TH --disable-binding --cluster-smp-presend 780 --cluster-node-memory $((1024*1024*(128))) --thd-output"
+
+if [ $NUM_TH == 1 ]; then
+   export NX_SCHEDULE=affinity-ready
+else
+   export NX_SCHEDULE=affinity
+fi
+echo "Using $NX_SCHEDULE scheduling policy"
+
+export EXTRAE_CONFIG_FILE=./extrae.xml
+export LD_BIND_NOW=1
+export LD_LIBRARY_PATH=/usr/lib/performance
+
+echo axiom-run -P ompss ./nbody $ARG1 $ARG3 0
+axiom-run -P ompss ./nbody $ARG1 $ARG3 0
+
+fi
