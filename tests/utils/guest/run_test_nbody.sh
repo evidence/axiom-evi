02bc957e6a2598c574821366ffd8c0f064a1ad28
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-01-27 12:08:17 +0100

    tests nbody: using 3 threads

--- tests/utils/guest/run_test_nbody.sh
+++ tests/utils/guest/run_test_nbody.sh
@@ -7,9 +7,9 @@ if [ x$1 == x ]; then
 else
     NUM_PART=$1
     NUM_STEP=$2
-    NUM_THREADS=1
+    NUM_THREADS=3
 
-    echo "Using file 'particles-${NUM_PART}-128-${NUM_STEP}.in'"        
+    echo "Using file 'particles-${NUM_PART}-128-${NUM_STEP}.in'"
     echo "Executing N-body on `axiom-info -N -q` boards..."
 
     #export NX_ARGS="--cluster --cluster-network axiom --smp-workers $NUM_THREADS --disable-binding --cluster-smp-presend 780 --thd-output"
@@ -24,7 +24,7 @@ else
 
     export LD_BIND_NOW=1
     export LD_LIBRARY_PATH=/usr/lib/performance
-        
+
     echo axiom-run -P ompss ./nbody $NUM_PART $NUM_STEP 0
     axiom-run -P ompss ./nbody $NUM_PART $NUM_STEP 0
 fi
--- tests/utils/guest/run_test_nbody_extrae.sh
+++ tests/utils/guest/run_test_nbody_extrae.sh
@@ -7,9 +7,9 @@ if [ x$1 == x ]; then
 else
     NUM_PART=$1
     NUM_STEP=$2
-    NUM_THREADS=1
+    NUM_THREADS=3
 
-    echo "Using file 'particles-${NUM_PART}-128-${NUM_STEP}.in'"        
+    echo "Using file 'particles-${NUM_PART}-128-${NUM_STEP}.in'"
     echo "Executing N-body on `axiom-info -N -q` boards..."
 
     #export NX_ARGS="--cluster --cluster-network axiom --instrumentation extrae --smp-workers $NUM_THREADS --disable-binding --cluster-smp-presend 780 --thd-output"
@@ -25,12 +25,12 @@ else
     export LD_BIND_NOW=1
     export LD_LIBRARY_PATH=/usr/lib/instrumentation
     export LD_PRELOAD=/usr/lib/libnanostrace.so
-    
+
     export EXTRAE_CONFIG_FILE=./extrae.xml
     export NX_EXTRAE_AVOID_MERGE=1
     export EXTRAE_HOME=/usr
     rm -rf set-0
-    
+
     echo axiom-run -P ompss ./nbody $NUM_PART $NUM_STEP 0
     axiom-run -P ompss ./nbody $NUM_PART $NUM_STEP 0
 fi
