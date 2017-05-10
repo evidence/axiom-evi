07f96fcb3630970b10a0eec08fa0cad04413b91f
Stefano Garzarella <s.garzarella@evidence.eu.com>
2017-05-10 11:56:50 +0200

    small fix in the init scripts

--- scripts/buildroot/overlay/etc/axiom_end_discovery.sh
+++ scripts/buildroot/overlay/etc/axiom_end_discovery.sh
@@ -5,7 +5,7 @@
 [ -e /dev/net ] || mkdir /dev/net
 [ -e /dev/net/tun ] || mknod /dev/net/tun c 10 200
 
-killall axiom-tuntap 2>/dev/null 1>/dev/null
+killall axiom-ethtap 2>/dev/null 1>/dev/null
 modprobe tun
 axiom-ethtap &
 
@@ -14,4 +14,4 @@ sleep 2
 ID=`axiom-info -n -q`
 ifconfig ax0 192.168.17.${ID}
 
-echo My IP is 192.168.17.${ID}
+echo "My IP is 192.168.17.${ID}"
--- scripts/buildroot/overlay/root/axiom-reload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-killall axiom-init
-rmmod axiom_netdev
-rmmod axiom_mem_dev
-rmmod axiom_mem_manager
-./axiom-startup.sh $1
