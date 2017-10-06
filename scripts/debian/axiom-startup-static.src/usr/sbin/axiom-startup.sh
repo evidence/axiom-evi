b4a3f34750d9f4203753ac9158f1c3b9644f5c5e
Stefano Garzarella <s.garzarella@evidence.eu.com>
2017-10-06 09:27:45 +0200

    axiom-startup.sh: autogenerate static routing table

--- scripts/debian/axiom-startup-static.src/usr/sbin/axiom-startup.sh
+++ scripts/debian/axiom-startup-static.src/usr/sbin/axiom-startup.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
 
-if [ x$1 == x ]; then
-    echo -e "\nUsage: $0 NODEID\n"
+NODES=2
+
+function usage () {
+    echo -e "\nUsage: $0 [-n NODES]  NODEID\n"
     echo -e "Load all AXIOM modules and start axiom-init and axiom-ethtap."
-    echo -e "NODEID is required, because for now we use static routing."
+    echo -e "NODEID and NODES are required, because for now we use static routing."
     echo -e "\nExample: $0 1\n"
+
+    echo -e "\noptions (specified before NODEID):\n"
+    echo -e "    -n  NODES   specify the number of nodes in the cluster [default $NODES]\n"
+    echo -e "    -h          print this help\n"
+
+}
+
+while getopts ":n:h" opt; do
+    case $opt in
+        n)
+            NODES=$OPTARG
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+
+done
+shift $((OPTIND-1))
+
+if [ x$1 == x ]; then
+    usage
     exit 1
 fi
+
+ID=$1
 
 # Make sure only root can run this script
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
-ID=$1
 
 pkill axiom-ethtap > /dev/null 2>&1
 pkill axiom-init > /dev/null 2>&1
@@ -30,7 +65,20 @@ echo 0 > /proc/sys/kernel/randomize_va_space
 modprobe axiom-netdev
 modprobe tun
 
-axiom-init -n $1 -r /etc/axiom/axiom_static_rt_node_${ID}.txt &
+# generate the static routing table
+RT_FILE="/tmp/axiom_static_rt.txt"
+
+echo "-1" > $RT_FILE
+
+for i in `seq 1 $NODES`; do
+    if [ "$i" -eq "$ID" ]; then
+        echo "0" >> $RT_FILE
+    else
+        echo "1" >> $RT_FILE
+    fi
+done
+
+axiom-init -n ${ID} -r ${RT_FILE} &
 
 sleep 1
 axiom-ethtap -n 8 &
