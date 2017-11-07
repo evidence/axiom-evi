b9712ad133c78d92f00a27d78ce82cfa2f010ad3
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-11-07 10:45:46 +0100

    Debian packages for axiom startup (sysv/systemd).

---
+++ scripts/debian_meta/etc/axiom/axiom_end_discovery.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# run by axiom-init at the end of discovery protocol
+
+killall axiom-ethtap 2>/dev/null 1>/dev/null
+modprobe tun
+axiom-ethtap -n 8 &
+
+sleep 2
+
+ID=`axiom-info -n -q`
+ifconfig ax0 192.168.17.${ID}
+
+echo "My IP is 192.168.17.${ID}"
