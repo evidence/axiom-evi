0f09c8ea0ad76015e83d4cb16929bf5c296aceba
Stefano Garzarella <s.garzarella@evidence.eu.com>
2017-11-09 14:36:58 +0100

    axiom: update CHANGELOG and files version to v0.15

--- tests/ompss/src/ompss_evimm.c
+++ tests/ompss/src/ompss_evimm.c
@@ -1,7 +1,7 @@
 /*!
  * \file ompss_evimm.c
  *
- * \version     v0.14
+ * \version     v0.15
  * \date        2016-02-13
  *
  * This example implements Matrix Multiplication on top of OmpSS in the same way
--- tests/ompss/src/time.h
+++ tests/ompss/src/time.h
@@ -1,7 +1,7 @@
 /*!
  * \file time.h
  *
- * \version     v0.14
+ * \version     v0.15
  * \date        2016-02-13
  *
  * Copyright (C) 2016, Evidence Srl.
