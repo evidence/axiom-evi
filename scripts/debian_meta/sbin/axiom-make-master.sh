ffa62ad215ebde772a8c4019a3abddd9582971c8
Stefano Garzarella <s.garzarella@evidence.eu.com>
2018-01-30 11:48:33 +0100

    deb: enable generation of axiom-startup package

--- scripts/debian_meta/sbin/axiom-make-master.sh
+++ scripts/debian_meta/sbin/axiom-make-master.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 
-[ -e /etc/axiom/axiomrc ] && sed -i "s/MASTER=no/MASTER=yes/" /etc/axiom/axiomrc
+#[ -e /etc/axiom/axiomrc ] && sed -i "s/MASTER=no/MASTER=yes/" /etc/axiom/axiomrc
+axiom-utility -m
