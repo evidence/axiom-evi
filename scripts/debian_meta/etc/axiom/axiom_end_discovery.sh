9a42cc8f740c0270409dce910a681d4c8f13aa8b
Stefano Garzarella <s.garzarella@evidence.eu.com>
2018-01-31 17:23:51 +0100

    axiom-end-discovery: set MTU to 65500

--- scripts/debian_meta/etc/axiom/axiom_end_discovery.sh
+++ scripts/debian_meta/etc/axiom/axiom_end_discovery.sh
@@ -3,12 +3,14 @@
 # run by axiom-init at the end of discovery protocol
 
 pkill axiom-ethtap 2>/dev/null 1>/dev/null
+sleep 1
+
 modprobe tun
 axiom-ethtap -n 8 &
 
-sleep 2
+sleep 1
 
 ID=`axiom-info -n -q`
-ifconfig ax0 192.168.17.${ID}
+ifconfig ax0 192.168.17.${ID} mtu 65500
 
 echo "My IP is 192.168.17.${ID}"
