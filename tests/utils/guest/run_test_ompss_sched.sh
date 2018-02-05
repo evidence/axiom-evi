ce434d0d7f78b554104d251eafe5d0eccd419c65
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2018-02-05 10:44:21 +0100

    deb ompss test: add scripts to run the test changing the scheduler

---
+++ tests/utils/guest/run_test_ompss_sched.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=/usr/lib/performance
+#export LD_LIBRARY_PATH=/usr/lib/debug
+
+set -x
+
+NX_ARGS="--num-sockets=1 --cpus-per-socket=5 --smp-cpus=5 --use-my-cpus-number=1 --cluster --cluster-network=axiom --cluster-smp-presend=4" \
+       NX_CLUSTER_NODE_MEMORY=67108864 \
+       NX_GASNET_SEGMENT_SIZE=67108864 \
+       AXIOM_WRK_PARAMS=OTHER,5 \
+       AXIOM_COM_PARAMS=DEADLINE,5%,100m,100m \
+       axiom-run -P ompss $@
