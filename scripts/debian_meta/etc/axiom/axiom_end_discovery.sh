aa4058ac79524c1cbad0215dfd00c6e092424cab
Stefano Garzarella <s.garzarella@evidence.eu.com>
2018-06-08 13:58:57 +0200

    axiom_end_discovery: set up NAT on master node

--- scripts/debian_meta/etc/axiom/axiom_end_discovery.sh
+++ scripts/debian_meta/etc/axiom/axiom_end_discovery.sh
@@ -16,3 +16,22 @@ ifconfig ax0 192.168.17.${ID} mtu 65500
 echo "IPoverAXIOM set to subnet 192.168.17.0/24"
 echo "Other nodes are reachable with IP: 192.168.17.NODEID"
 echo "Local IP is 192.168.17.${ID}"
+
+# setup NAT on master node and gateway and DNS on slave nodes
+modprobe iptable_nat
+if [ "$?" == "0" ]; then
+    # master node
+    if [ "${ID}" == "1" ]; then
+        sysctl -w net.ipv4.ip_forward=1
+        iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+        iptables -A FORWARD -i eth0 -o ax0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        iptables -A FORWARD -i ax0 -o eth0 -j ACCEPT
+    else #slave node
+        # set default gateway
+        ip route add default via 192.168.17.1
+        # set google DNS
+        echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > /etc/resolvconf/resolv.conf.d/tail
+        resolvconf -u
+    fi
+    echo "NAT enabled. Internet reachable through Master node ethernet port"
+fi
